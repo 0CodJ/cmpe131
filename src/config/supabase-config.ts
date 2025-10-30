4a11c254c215870b56d8de787c1e11f2cf7b57b6
Walnut_J <codyjambrosio@gmail.com>
2025-10-29 19:23:40 -0700

    Update supabase-config.ts

--- src/config/supabase-config.ts
+++ src/config/supabase-config.ts
@@ -3,6 +3,6 @@
 // Keep .env file for future use in case the issue gets resolved
 
 export const supabaseConfig = {
-  url: import.meta.env.VITE_SUPABASE_URL || 'https://wvveeyfcfstpotptvjen.supabase.co',
-  anonKey: import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2dmVleWZjZnN0cG90cHR2amVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNzE2NDgsImV4cCI6MjA3NjY0NzY0OH0.L4b6g8pQlwSwWhM-wP9OGsPOMlkBvbgbYMXgb-P_o6M'
+  url: import.meta.env.VITE_SUPABASE_URL || 'https://(insert code for supabase).supabase.co',
+  anonKey: import.meta.env.VITE_SUPABASE_ANON_KEY || '(insert api key here)'
 };
